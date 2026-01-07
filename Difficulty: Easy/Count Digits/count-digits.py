e6e56710ae18ba58d7f17e46d5eafc6661418c3a
Mohd Zaheeruddin <183212672+mdzaheerjk@users.noreply.github.com>
2026-01-07 22:56:38 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Count Digits/count-digits.py
@@ -0,0 +1,10 @@
+class Solution:
+    def countDigits(self, n):
+        if n==0:
+            return 1
+    #  code here 
+        count=0
+        while n>0:
+            n=n//10
+            count+=1
+        return count
